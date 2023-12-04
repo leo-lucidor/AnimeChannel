b6cd3ea67508b806bce87cdcc8258254210ebc34
Blandeau Erwan <erwan.blandeau28@gmail.com>
2023-12-04 14:15:38 +0100

    changement

--- api/requete.py
+++ api/requete.py
@@ -28,7 +28,72 @@ class Utilisateur:
                 res.append(row)
             return result
         
+
+
+class Anime:
+    class Get:
+        def get_all_anime(cnx):
+            res =  []
+            result = cnx.execute(text("select * from Anime;"))
+            for row in result:
+                print(row)
+                res.append(row)
+            return result
+        
+        def get_anime_by_id(cnx, id):
+            res =  []
+            result = cnx.execute(text("select * from Anime where idAnime = :id;"), id=id)
+            for row in result:
+                print(row)
+                res.append(row)
+            return result
+        
+        def get_anime_by_name(cnx, name):
+            res =  []
+            result = cnx.execute(text("select * from Anime where nameA = :name;"), name=name)
+            for row in result:
+                print(row)
+                res.append(row)
+            return result
         
 
+class Episode:
+    class Get:
+        def get_all_episode(cnx):
+            res =  []
+            result = cnx.execute(text("select * from Episode;"))
+            for row in result:
+                print(row)
+                res.append(row)
+            return result
+        
+        def get_episode_by_id(cnx, id):
+            res =  []
+            result = cnx.execute(text("select * from Episode where idEpisode = :id;"), id=id)
+            for row in result:
+                print(row)
+                res.append(row)
+            return result
+        
+        def get_episode_by_name(cnx, name):
+            res =  []
+            result = cnx.execute(text("select * from Episode where nameE = :name;"), name=name)
+            for row in result:
+                print(row)
+                res.append(row)
+            return result
+        
+        def get_episode_by_id_and_numeroEpisode(cnx, id, numeroEpisode):
+            res =  []
+            result = cnx.execute(text("select * from Episode where idAnime = " + str(id) + " and numeroEpisode = " + str(numeroEpisode) + ";"))
+            for row in result:
+                print(row)
+                res.append(row)
+            return result
+        
 
-Utilisateur.Get.get_all_utilisateur(cnx)
+# Utilisateur.Get.get_all_utilisateur(cnx)
+# Anime.Get.get_all_anime(cnx)
+# Episode.Get.get_all_episode(cnx)
+# Episode.Get.get_episode_by_id_and_numeroEpisode(cnx, 1, 1)
+
